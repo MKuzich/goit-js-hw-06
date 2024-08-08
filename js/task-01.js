aab8d272bf0f5d86caef5dfa7e9efc80ea90be9d
Mykhailo Kuzich <timmkedth@gmail.com>
2024-08-08 19:00:01 +0200

    Completed

--- js/task-01.js
+++
@@ -1,6 +0,0 @@
-const items = document.querySelectorAll('.item');
-console.log('Number of categories:', items.length);
-items.forEach( (item) => {
-    console.log('Category:', item.firstElementChild.textContent);
-    console.log('Elements:', item.lastElementChild.children.length);
-});
--- js/task-07.js
+++
@@ -1,6 +0,0 @@
-const input = document.querySelector('#font-size-control');
-const text = document.querySelector('#text')
-function onInputChange(event) {
-    return text.style.fontSize = `${event.currentTarget.value}px`;
-}
-input.addEventListener('input', onInputChange);
